5fd8f5cc585fcc82cb832fc30fada2c2945d2596
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2024-09-05 23:30:26 -0300

    Correção pull

--- PrimeiraClass/src/EstudandoClasse/EstudandoClass.java
+++ PrimeiraClass/src/EstudandoClasse/EstudandoClass.java
@@ -3,8 +3,8 @@ package EstudandoClasse;
 public class EstudandoClass {
     public static void main(String[] args) {
         System.out.println("Minha primeira class!");
-        System.out.println("Meu primeirio commit");
-        System.out.println("Testando pull"):
+        System.out.println("Meu primeirio commit!");
+        System.out.println("Testando pull!");
             
 
     }
