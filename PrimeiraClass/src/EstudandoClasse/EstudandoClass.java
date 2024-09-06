f6776b7fe7b385a90290fdd823d4419ac84190ff
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2024-09-05 23:24:54 -0300

    Update EstudandoClass.java
    Testando pull

--- PrimeiraClass/src/EstudandoClasse/EstudandoClass.java
+++ PrimeiraClass/src/EstudandoClasse/EstudandoClass.java
@@ -4,6 +4,8 @@ public class EstudandoClass {
     public static void main(String[] args) {
         System.out.println("Minha primeira class!");
         System.out.println("Meu primeirio commit");
+        System.out.println("Testando pull"):
+            
 
     }
 
