ad978ed04c10c2761b1287d85e872b4f94536735
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2024-10-05 16:12:19 -0300

    Commit do dia

--- PrimeiraClass/src/EstudandoPoo/ProjetoAluno/Test/AlunoTest.java
+++ PrimeiraClass/src/EstudandoPoo/ProjetoAluno/Test/AlunoTest.java
@@ -34,9 +34,12 @@ public class AlunoTest {
         aluno1.setSerieMatriculada(serieMatriculada);
 
         for (int pos = 1; pos <= 4; pos++) {
+
             String nomeDisciplina = JOptionPane.showInputDialog("Qual o nome da disciplina " + pos + " ?");
             String notaDisciplina = JOptionPane.showInputDialog("Qual foi a nota desta disciplina " + pos + "?");
+
             Disciplina disciplina = new Disciplina();
+            disciplina.setDisciplina(nomeDisciplina);
             disciplina.setNota(Double.valueOf(notaDisciplina));
 
             aluno1.getDisciplinas().add(disciplina);
@@ -44,13 +47,27 @@ public class AlunoTest {
         }
 
         int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
-        if (escolha == 0) {
-            String disciplinaRemover = JOptionPane
-                    .showInputDialog("Qual a disciplina deseja remover ?  [1] [2] [3] ou [4]");
-            aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - 1);
+
+        if (escolha == 0) {// "Opção SIM e ZERO"
+
+            int continuarRemover = 0;
+
+            while (continuarRemover == 0) {
+
+                String disciplinaRemover = JOptionPane
+                        .showInputDialog("Qual a disciplina deseja remover ?  [1] [2] [3] ou [4]");
+                aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - 1);
+
+                continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover? ");
+
+                if
+
+
+
+            }
 
         }
-        System.out.println(aluno1);//Descrição do objeto na memoria
+        System.out.println(aluno1);// Descrição do objeto na memoria
         System.out.println("A media de sua nota foi: " + aluno1.getMediaNota());
         System.out.println(" Resultado: " + aluno1.getResultadoFinal());
 
