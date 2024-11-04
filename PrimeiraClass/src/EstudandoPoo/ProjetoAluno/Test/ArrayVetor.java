46cc415967fce5268f855ad428bccfa97bd3f620
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2024-11-04 11:48:01 -0300

    Atualização

--- PrimeiraClass/src/EstudandoPoo/ProjetoAluno/Test/ArrayVetor.java
+++ PrimeiraClass/src/EstudandoPoo/ProjetoAluno/Test/ArrayVetor.java
@@ -20,7 +20,7 @@ public class ArrayVetor {
 
         // Criação da Disciplina
         Disciplina disciplina = new Disciplina();
-        disciplina.setDisciplina("Cueso de Java");
+        disciplina.setDisciplina("Curso de Java");
         disciplina.setNota(notas);
 
         aluno.getDisciplinas().add(disciplina);
@@ -31,71 +31,98 @@ public class ArrayVetor {
 
         aluno.getDisciplinas().add(disciplina2);
 
-        System.out.println("Nome : " + aluno.getNome() + " / " + " Inscrito no curso : " + aluno.getNomeEscola());
+        // -----------------------------------------------------------------------------
 
-        for (Disciplina d : aluno.getDisciplinas()) {
+        Aluno[] arrayAlunos = new Aluno[1];
 
-            System.out.println("---------- Disciplina do aluno ----------");
-            System.out.println("Disciplina : " + d.getDisciplina());
-            System.out.println("As notas das disciplinas são : ");
+        arrayAlunos[0] = aluno;
 
-            double notaMax = 0.0;
-            double notaMin = 10;
+        for (int pos = 0; pos < arrayAlunos.length; pos++) {
 
-            // Regras de negocio para descobrir a MAIOR e MENOR nota do aluno.
-            for (int pos = 0; pos < d.getNota().length; pos++) {
+            System.out.println("O nome do aluno é: " + arrayAlunos[pos].getNome());
 
-                System.out.println("Nota  " + pos + " E igual = " + d.getNota()[pos]);
+            for (Disciplina d : arrayAlunos[pos].getDisciplinas()) {
 
-                if (pos == 0) {
+                System.out.println("O nome da discplina é: " + d.getDisciplina());
 
-                    notaMax = d.getNota()[pos];
+                for (int posnota = 0; posnota < d.getNota().length; posnota++) {
+                    
 
-                } else {
-                    if (d.getNota()[pos] > notaMax) {
-                        notaMax = d.getNota()[pos];
-                    }
+                    System.out.println("Nota: " + posnota + " é igual = " + d.getNota()[posnota]);
                 }
-
-                if (pos == 0) {
-
-                    notaMin = d.getNota()[pos];
-                } else {
-                    if (d.getNota()[pos] < notaMin)
-                        notaMin = d.getNota()[pos];
-
-                }
-
             }
-            System.out.println("A maior nota da disciplina = " + d.getDisciplina() + " e de valor : " + notaMax);
-            System.out.println("A menor nota da disciplina = " + d.getDisciplina() + " e de valor : " + notaMin);
+
         }
 
-        /*
-         * Array pode ser de todos os tipos de dados e objetos também.
-         * 
-         * String posicoes =
-         * JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
-         * 
-         * Array deve ter quantidade de posições definidas
-         * double notas[] = new double[Integer.parseInt(posicoes)];// Interger.parseInt
-         * Recebe uma string e retorna para
-         * // inteiro
-         * 
-         * for (int pos = 0; pos < notas.length; pos++) {/* Length significa que o
-         * calculo le o tamanho do Array
-         * String valor = JOptionPane.showInputDialog("Qual o valor da posição? " +
-         * pos);
-         * notas[pos] = Double.valueOf(valor);
-         * 
-         * }
-         * 
-         * 
-         * for (int pos = 0; pos < notas.length; pos++){
-         * 
-         * System.out.println("Nota " + (pos+1) + " é " + notas[pos]);
-         * }
-         */
     }
-
 }
+
+/*
+ * System.out.println("Nome : " + aluno.getNome() + " / " +
+ * " Inscrito no curso : " + aluno.getNomeEscola());
+ * 
+ * for (Disciplina d : aluno.getDisciplinas()) {
+ * 
+ * System.out.println("---------- Disciplina do aluno ----------");
+ * System.out.println("Disciplina : " + d.getDisciplina());
+ * System.out.println("As notas das disciplinas são : ");
+ * 
+ * double notaMax = 0.0;
+ * double notaMin = 10;
+ * 
+ * // Regras de negocio para descobrir a MAIOR e MENOR nota do aluno.
+ * for (int pos = 0; pos < d.getNota().length; pos++) {
+ * 
+ * System.out.println("Nota  " + pos + " E igual = " + d.getNota()[pos]);
+ * 
+ * if (pos == 0) {
+ * 
+ * notaMax = d.getNota()[pos];
+ * 
+ * } else {
+ * if (d.getNota()[pos] > notaMax) {
+ * notaMax = d.getNota()[pos];
+ * }
+ * }
+ * 
+ * if (pos == 0) {
+ * 
+ * notaMin = d.getNota()[pos];
+ * } else {
+ * if (d.getNota()[pos] < notaMin)
+ * notaMin = d.getNota()[pos];
+ * 
+ * }
+ * 
+ * }
+ * System.out.println("A maior nota da disciplina = " + d.getDisciplina() +
+ * " e de valor : " + notaMax);
+ * System.out.println("A menor nota da disciplina = " + d.getDisciplina() +
+ * " e de valor : " + notaMin);
+ * }
+ * 
+ * /*
+ * Array pode ser de todos os tipos de dados e objetos também.
+ * 
+ * String posicoes =
+ * JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
+ * 
+ * Array deve ter quantidade de posições definidas
+ * double notas[] = new double[Integer.parseInt(posicoes)];// Interger.parseInt
+ * Recebe uma string e retorna para
+ * // inteiro
+ * 
+ * for (int pos = 0; pos < notas.length; pos++) {/* Length significa que o
+ * calculo le o tamanho do Array
+ * String valor = JOptionPane.showInputDialog("Qual o valor da posição? " +
+ * pos);
+ * notas[pos] = Double.valueOf(valor);
+ * 
+ * }
+ * 
+ * 
+ * for (int pos = 0; pos < notas.length; pos++){
+ * 
+ * System.out.println("Nota " + (pos+1) + " é " + notas[pos]);
+ * }
+ */
