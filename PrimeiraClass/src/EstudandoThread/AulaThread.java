5a4a7c98c19d2175cfa485c0b256bedaa9e41583
Alexandro OLiveira <lecooliveirastartdev@hotmail.com>
2025-02-06 18:19:29 -0300

    Update AulaThread.java
    continua

--- PrimeiraClass/src/EstudandoThread/AulaThread.java
+++ PrimeiraClass/src/EstudandoThread/AulaThread.java
@@ -1,15 +1,36 @@
 package EstudandoThread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
+
     public static void main(String[] args) throws InterruptedException {
 
-        for (int pos = 0; pos < 10; pos++) {
+        new Thread() {
 
-            // Quero executar este envio com tempo de parada, ou com tempo determinado
-            System.out.println("Executando alguma rotina, por exemplo enviar email");
-            Thread.sleep(1000); // Da um tempo determinado para executar cada linha
-        }
+            public void run() {/* Executa o que nos queremos */
 
-        System.out.println("----------------Fim da execução de Teste --------------------------------");
+                /* Codigo da rotina que eu quero executar em paraleelo */
+                for (int pos = 0; pos < 10; pos++) {
+
+                    // Quero executar este envio com tempo de parada, ou com tempo determinado
+                    System.out.println("Executando alguma rotina, por exemplo enviar email");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } // Da um tempo determinado para executar cada linha
+                }
+                /* Fim do codigo */
+
+            };
+            
+            /* Codigo da rotina */
+        }.start(); /* Liga a Tread que fica processando paralelamente */
+
+        /* Codigo do sistema do usuário continua fluxo de trabalho */
+        System.out.println("---------------- Thread em segundo plano --------------------------------");
+        /* Fluxo do sistema */
+        JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
     }
 }
